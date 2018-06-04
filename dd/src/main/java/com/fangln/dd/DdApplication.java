5e5082bda807d6633ac9ef570308ed71610ace75
fanglinan <971399161@qq.com>
2018-06-04 21:51:31 +0800

    init

---
+++ dd/src/main/java/com/fangln/dd/DdApplication.java
@@ -0,0 +1,12 @@
+package com.fangln.dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdApplication.class, args);
+    }
+}
