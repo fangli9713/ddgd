8a94b8e1cc5f2d4c5ddd8bddb971285e013b3e63
971399161@qq.com <971399161@qq.com>
2018-06-05 13:53:08 +0800

    init

--- dd/src/main/java/com/fangln/dd/DdApplication.java
+++ dd/src/main/java/com/fangln/dd/DdApplication.java
@@ -1,9 +1,11 @@
 package com.fangln.dd;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.fangln.dd.dao")
 public class DdApplication {
 
     public static void main(String[] args) {
